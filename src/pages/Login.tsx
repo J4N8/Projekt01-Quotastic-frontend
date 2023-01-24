705486862d3291a1e6cc1f557ab63a52810f9686
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-01-24 14:08:53 +0100

    Created dashboard layout with all pages and routes

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+
+import Layout from "../components/ui/Layout";
+
+const Login: FC = () => {
+	return (
+		<Layout>
+			{/*<LoginForm />*/}
+			Login
+		</Layout>
+	);
+};
+
+export default Login;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from "react";
+import Layout from "../components/ui/Layout";
+
+const Register: FC = () => {
+	return (
+		<Layout>
+			{/*<RegisterForm />*/}
+			Register
+		</Layout>
+	);
+};
+
+export default Register;
