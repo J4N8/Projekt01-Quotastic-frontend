4a406067548f58b230313361252e2de06ab5a4ec
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-02-03 12:06:18 +0100

    Created basic layout for landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,38 @@
 import Layout from "components/ui/Layout";
 import {FC} from "react";
+import {routes} from "../constants/routesConstants";
+import {NavLink} from "react-router-dom";
 
 const Home: FC = () => {
 	return (
 		<Layout>
 			<div className="p-2 mb-4">
 				<div className="container-fluid py-4">
-					<h1 className="display-5 fw-bold">Welcome</h1>
-					<p className="col-md-8 fs-4">In this tutorial you will learn how to use:</p>
-					<ol>
-						<li>react-bootstrap</li>
-						<li>react-hook-form with yup validation</li>
-						<li>CRUD functionality - Create Read Update Delete</li>
-						<li>file upload with NestJS</li>
-						<li>connect ReactJS and NestJS</li>
-						<li>setup NestJS with custom logging</li>
-						<li>connect to PostgreSQL database using TypeORM</li>
-						<li>create dtos with validation</li>
-						<li>deploy ReactJS to AWS S3 Bucket</li>
-						<li>deploy NestJS to AWS ECS (Elastic Container Service)</li>
-					</ol>
+					<h1>Welcome to Quotastic</h1>
+					<p>
+						Quotastic is free online platform for you to explore the quips, quotes and proverbs. Sign up and
+						express yourself.
+					</p>
+					<NavLink className="nav-link pe-0" to={routes.SIGNUP}>
+						Signup
+					</NavLink>
+				</div>
+				<div>
+					<img src="landing_page_quotes.png" />
+				</div>
+
+				<div>
+					<h1>Explore the world of fantastic quotes</h1>
+					<br />
+					<h3>Most upvoted quotes</h3>
+					<p>
+						Most upvoted quotes on the platform. Sign up or login to like the quotes and keep them saved in
+						your profile
+					</p>
+					{/*	TODO: QUOTES SORTED BY VOTE SCORE*/}
+					<NavLink className="nav-link pe-0" to={routes.SIGNUP}>
+						Sign up to see more
+					</NavLink>
 				</div>
 			</div>
 		</Layout>
