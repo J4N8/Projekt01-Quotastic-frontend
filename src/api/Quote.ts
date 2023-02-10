5f7e813ef2c79b745a7bde02130c4335376bd97c
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-02-10 13:17:24 +0100

    Fixed quote API paths

--- src/api/Quote.ts
+++ src/api/Quote.ts
@@ -8,11 +8,14 @@ export const fetchQuotes = async (pageNumber: number) =>
 	apiRequest<undefined, QuoteType[]>("get", `${apiRoutes.QUOTES_PREFIX}?page=${pageNumber}`);
 
 export const createQuote = async (data: CreateUpdateQuoteFields) =>
-	apiRequest<CreateUpdateQuoteFields, QuoteType>("post", `/me/myquote`, data);
+	apiRequest<CreateUpdateQuoteFields, QuoteType>("post", `${apiRoutes.QUOTES_PREFIX}`, data);
 
 export const updateQuote = async (data: CreateUpdateQuoteFields, id: string) =>
 	apiRequest<CreateUpdateQuoteFields, QuoteType>("patch", `${apiRoutes.QUOTES_PREFIX}/${id}`, data);
 
-export const deleteQuote = async (id: string) => apiRequest<string, QuoteType>("delete", `me/myquote/${id}`);
-export const upvoteQuote = async (id: string) => apiRequest<string, QuoteType>("post", `quotes/${id}/upvote`);
-export const downvoteQuote = async (id: string) => apiRequest<string, QuoteType>("post", `quotes/${id}/downvote`);
+export const deleteQuote = async (id: string) =>
+	apiRequest<string, QuoteType>("delete", `${apiRoutes.QUOTES_PREFIX}/${id}`);
+export const upvoteQuote = async (id: string) =>
+	apiRequest<string, QuoteType>("post", `${apiRoutes.QUOTES_PREFIX}/${id}/upvote`);
+export const downvoteQuote = async (id: string) =>
+	apiRequest<string, QuoteType>("post", `${apiRoutes.QUOTES_PREFIX}/${id}/downvote`);
