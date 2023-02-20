06d8ad6ef8d44c2bffc29c57b6b3c93d9e2f573e
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-02-20 12:35:44 +0100

    Mobile design improvements

--- src/components/homepage/HomeNotAuthenticated.tsx
+++ src/components/homepage/HomeNotAuthenticated.tsx
@@ -63,7 +63,7 @@ const HomeNotAuthenticated = () => {
 							) : (
 								<>
 									<div className="d-flex container">
-										<div className="row row-cols-4">
+										<div className={isMobile ? "row row-cols-1" : "row row-cols-4"}>
 											{sortedScore.data?.data.data.map((item: QuoteType, index: number) => (
 												<div className="p-2 col border-orange m-1">
 													<Quote quoteValues={item} key={index} />
--- src/components/ui/NavbarNoLogin.tsx
+++ src/components/ui/NavbarNoLogin.tsx
@@ -1,8 +1,11 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import {Link, NavLink, useNavigate} from "react-router-dom";
 import {routes} from "../../constants/routesConstants";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 const Navbar: FC = () => {
+	const {isMobile} = useMediaQuery(768);
+	const [navbarToggle, setNavbarToggle] = useState(false);
 	useNavigate();
 	return (
 		<>
@@ -12,17 +15,26 @@ const Navbar: FC = () => {
 						<Link className="navbar-brand" to={routes.HOME}>
 							<img src="/images/logo.png" alt="SkillUp Mentor" width={123} />
 						</Link>
+						<button className="navbar-toggler" type="button" onClick={() => setNavbarToggle(!navbarToggle)}>
+							<span className="navbar-toggler-icon"></span>
+						</button>
 						<div
-							className="collapse navbar-collapse justify-content-end align-items-center"
+							className={
+								navbarToggle
+									? "navbar-collapse justify-content-end align-items-center"
+									: "collapse navbar-collapse justify-content-end align-items-center"
+							}
 							id="navbarTogglerDemo02"
 						>
-							<ul className="navbar-nav mb-2 mb-lg-0">
-								<li className="nav-item pe-4">
+							<ul
+								className={isMobile ? "navbar-nav mb-2 mb-lg-0 text-center" : "navbar-nav mb-2 mb-lg-0"}
+							>
+								<li className={isMobile ? "nav-item pe-4" : "nav-item pe-4"}>
 									<NavLink className="nav-link component-orange rounded-4" to={routes.LOGIN}>
 										Login
 									</NavLink>
 								</li>
-								<li className="nav-item">
+								<li className={isMobile ? "nav-item pe-4" : "nav-item pe-4"}>
 									<NavLink className="nav-link component-orange rounded-4" to={routes.SIGNUP}>
 										Signup
 									</NavLink>
